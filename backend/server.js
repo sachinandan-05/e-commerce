3156d052c882c3c917c64a3cbe65772caffae424
sachinandan <sachii05@Sachinandans-MacBook-Air.local>
2025-07-29 00:16:04 +0530

    fix deployement

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ const adminOrderRoutes = require("./routes/adminOrderRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true 
+}));
 
 dotenv.config();
 
